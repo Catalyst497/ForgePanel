3d5fc3bc0c94b355b1c4c31d7508a1ffbef0d2ff
Catalyst <danielduyilemi@gmail.com>
2025-05-13 02:18:33 +0100

    Do door selections.

--- app/components/Samples.tsx
+++ app/components/Samples.tsx
@@ -12,68 +12,89 @@ type Door = {
   location?: string;
 };
 
-
 const Samples = () => {
-  const { activePosition, doorPositions } = useAppStore();
+  const { activePosition, setActivePosition, doorPositions } = useAppStore();
   const [doorResults, setDoorResults] = useState<any>([]);
+  const [viewType, setViewType] = useState(1);
 
+  function selectOneDoorPerType(doors: Door[]): Door[] {
+    const seenTypes = new Set();
+    const result: Door[] = [];
 
-  
-  function selectOneDoorPerType(doors:Door[]): Door[]{
-  const seenTypes = new Set();
-  const result: Door[] = [];
-
-  for (const door of doors) {
-    if (!seenTypes.has(door.type)) {
-      seenTypes.add(door.type);
-      result.push(door);
+    for (const door of doors) {
+      if (!seenTypes.has(door.type)) {
+        seenTypes.add(door.type);
+        result.push(door);
+      }
     }
+
+    return result;
   }
-
-  return result;
-}
-
 
   useEffect(() => {
     if (activePosition !== null) {
+      setViewType(1);
       if (doorPositions[activePosition] === "Exterior") {
         const selectedDoors = doorTypes.filter((door) => {
-          if (door.material === "pocket" || door.material === "barn" || door.material === "bifold") {
-            return false
+          if (
+            door.type === "pocket" ||
+            door.type === "barn" ||
+            door.type === "bifold"
+          ) {
+            return false;
           } else return true;
-        })
-        setDoorResults(selectOneDoorPerType(selectedDoors))
+        });
+        setDoorResults(selectOneDoorPerType(selectedDoors));
       }
       if (doorPositions[activePosition] === "Interior") {
-        setDoorResults(selectOneDoorPerType(doorTypes))
+        setDoorResults(selectOneDoorPerType(doorTypes));
       }
     }
   }, [activePosition]);
 
+  const handleTypeClick = (e: any, door: Door) => {
+    setDoorResults(doorTypes.filter((lekun) => lekun.type === door.type));
+    setActivePosition(null);
+    setViewType(2);
+  };
   // useMemo(() => {console.log(doorResults)}, [doorResults])
   return (
     <div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-        {doorResults.length > 0 ? (
-          doorResults.map((door: any, index: number) => (
-            <div
-              key={index}
-              className="relative  shadow-md overflow-hidden transition-transform hover:scale-105 w-[10rem]"
-            >
-              <img
-                src={door.image}
-                alt={door.name}
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-2 absolute inset-0 top-auto backdrop-blur-lg">
-                <h3 className="text-lg font-medium">{door.type}</h3>
-              </div>
-            </div>
-          ))
-        ) : (
-          <h2>Please make a selection, and let's get started.</h2>
-        )}
-      </div>
+      {doorResults.length > 0 ? (
+        <div>
+          {activePosition !== null && (
+            <h2 className="py-2 font-bold">
+              What kind of door are we going for?
+            </h2>
+          )}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {doorResults.map((door: Door, index: number) => {
+              return (
+                <div
+                  key={index}
+                  onClick={(e) => handleTypeClick(e, door)}
+                  className="relative  shadow-md overflow-hidden transition-transform duration-300 ease-in-out hover:scale-105 w-[15rem]"
+                >
+                  <img
+                    src={door.image}
+                    alt={door.name}
+                    className="w-full h-[15rem] object-cover"
+                  />
+                  {viewType === 1 && (
+                    <div className="p-2 absolute inset-0 top-auto bg-black/15 backdrop-blur-lg">
+                      <h3 className="text-lg font-medium capitalize">
+                        {door.type} door
+                      </h3>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ) : (
+        <h2>Please make a selection, and let's get started.</h2>
+      )}
     </div>
   );
 };
